c2ca32fc32377c94431a6d6424efc10cf1f8d50d
Valery Piashchynski <piashchynski.valery@gmail.com>
2022-04-30 21:34:07 +0200

    fix: linters
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- amqpjobs/listener.go
+++ amqpjobs/listener.go
@@ -9,48 +9,42 @@ import (
 
 func (c *Consumer) listener(deliv <-chan amqp.Delivery) {
 	go func() {
-		for { //nolint:gosimple
-			select {
-			case msg, ok := <-deliv:
-				if !ok {
-					c.log.Debug("delivery channel was closed, leaving the rabbit listener")
-					// reduce number of listeners
-					if atomic.LoadUint32(&c.listeners) == 0 {
-						c.log.Debug("number of listeners", zap.Uint32("listeners", atomic.LoadUint32(&c.listeners)))
-						return
-					}
-
-					atomic.AddUint32(&c.listeners, ^uint32(0))
-					c.log.Debug("number of listeners", zap.Uint32("listeners", atomic.LoadUint32(&c.listeners)))
-					return
-				}
-
-				d, err := c.fromDelivery(msg)
+		for msg := range deliv {
+			d, err := c.fromDelivery(msg)
+			if err != nil {
+				c.log.Error("delivery convert", zap.Error(err))
+				/*
+					Acknowledge failed job to prevent endless loo;
+				*/
+				err = msg.Ack(false)
 				if err != nil {
-					c.log.Error("delivery convert", zap.Error(err))
-					/*
-						Acknowledge failed job to prevent endless loo;
-					*/
-					err = msg.Ack(false)
-					if err != nil {
-						c.log.Error("nack failed", zap.Error(err))
-					}
-
-					if d != nil {
-						d.Headers = nil
-						d.Options = nil
-					}
-					continue
+					c.log.Error("nack failed", zap.Error(err))
 				}
 
-				if d.Options.AutoAck {
-					// we don't care about error here, since the job is not important
-					_ = msg.Ack(false)
+				if d != nil {
+					d.Headers = nil
+					d.Options = nil
 				}
-
-				// insert job into the main priority queue
-				c.pq.Insert(d)
+				continue
 			}
+
+			if d.Options.AutoAck {
+				// we don't care about error here, since the job is not important
+				_ = msg.Ack(false)
+			}
+
+			// insert job into the main priority queue
+			c.pq.Insert(d)
 		}
+
+		c.log.Debug("delivery channel was closed, leaving the rabbit listener")
+		// reduce number of listeners
+		if atomic.LoadUint32(&c.listeners) == 0 {
+			c.log.Debug("number of listeners", zap.Uint32("listeners", atomic.LoadUint32(&c.listeners)))
+			return
+		}
+
+		atomic.AddUint32(&c.listeners, ^uint32(0))
+		c.log.Debug("number of listeners", zap.Uint32("listeners", atomic.LoadUint32(&c.listeners)))
 	}()
 }
